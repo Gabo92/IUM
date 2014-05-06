1c1e6f2e8a01bf038b5064012ac88a369fa60622
Groza Sergiu <serioja90@gmail.com>
2014-05-06 11:06:20 +0200

    Fatto il merge delle due versioni.

--- SitoIUM/js/data-manager.js
+++ SitoIUM/js/data-manager.js
@@ -15,6 +15,7 @@
     var DataManagerPrototype = Object.defineProperties({},{
        init: { writtable: false, configurable: false, enumerable: false,
            value: function(){
+               this.getAllData();
            }
        },
        trigger: { writtable: false, configurable: false, enumerable: false,
@@ -27,14 +28,21 @@
        },
        getAllData: { writtable: false, configurable: false, enumerable: false,
            value: function(){
+               var that = this;
                $(DataManager.NAMES).each(function(){
-                   
+                   getRowsFromVeryBeginningToNow(this,"tpellegr","DESC",that.onDataRecieved,that.onError);
                });
            }
        },
        onDataRecieved: { writtable: false, configurable: false, enumerable: false,
            value: function(xml){
-               consle.log(xml);
+            $(xml).find('Riga').each(function(){
+                             var Nome = $(this).find('Nome').text();
+                             var Valore = $(this).find('Valore').text();
+                             var Data = $(this).find('Data').text();
+                             var Unita = $(this).find('Unita').text();
+
+            console.log("Nome: "+ Nome + "\nValore: " + Valore + "\nUnita " + Unita + "\nData: "+ Data);})
            }
        },
        onError: { writtable: false, configurable: false, enumerable: false,
