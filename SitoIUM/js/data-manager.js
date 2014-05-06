3c37a5b343bda3d462c70a43e0357f1508bc1a91
Groza Sergiu <serioja90@gmail.com>
2014-05-06 10:42:36 +0200

    Creati i metodi di base per il caricamento e la gestione dei dati

--- SitoIUM/js/data-manager.js
+++ SitoIUM/js/data-manager.js
@@ -1,10 +1,38 @@
 (function(){
-    var DataManager = function(){
-        
+    var DataManager = function(options){
+        var configs = options || {};
+        this.events = configs["enevts"] || {};
+        this.init();
     };
     
     var DataManagerPrototype = Object.defineProperties({},{
-       
+       init: { writtable: false, configurable: false, enumerable: false,
+           value: function(){
+           }
+       },
+       trigger: { writtable: false, configurable: false, enumerable: false,
+           value: function(name,data){
+               var handler = this.events[name + ''];
+               if(handler && typeof handler === "function" ){
+                   handler(data);
+               }
+           }
+       },
+       getAllData: { writtable: false, configurable: false, enumerable: false,
+           value: function(){
+               
+           }
+       },
+       onDataRecieved: { writtable: false, configurable: false, enumerable: false,
+           value: function(xml){
+               
+           }
+       },
+       onError: { writtable: false, configurable: false, enumerable: false,
+           value: function(statusError){
+               console.error(statusError);
+           }
+       }
     });
     
     DataManager.prototype = DataManagerPrototype;
