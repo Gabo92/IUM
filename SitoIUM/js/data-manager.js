9db8cfff139e66df8921ab9e0d0d5fca5563cb13
Groza Sergiu <serioja90@gmail.com>
2014-05-06 11:01:15 +0200

    Apportate modifiche a DataManager.js

--- SitoIUM/js/data-manager.js
+++ SitoIUM/js/data-manager.js
@@ -2,8 +2,15 @@
     var DataManager = function(options){
         var configs = options || {};
         this.events = configs["enevts"] || {};
+        this.responsesCount = 0;
         this.init();
     };
+    
+    DataManager.NAMES = [
+        "AcquaConsumata","EnergiaConsumata","VotoPRS","VotoFCS","VotoPDS","VotoPRS",
+        "VotoFCS", "VotoPDS", "FondiInnovazione", "DocumentiDigitali", "TrasportoSostenibile",
+        "Orelavoro", "VotoStudenti", "RifiutiRiciclati", "RifornimentiEco"
+    ];
     
     var DataManagerPrototype = Object.defineProperties({},{
        init: { writtable: false, configurable: false, enumerable: false,
@@ -20,12 +27,14 @@
        },
        getAllData: { writtable: false, configurable: false, enumerable: false,
            value: function(){
-               
+               $(DataManager.NAMES).each(function(){
+                   
+               });
            }
        },
        onDataRecieved: { writtable: false, configurable: false, enumerable: false,
            value: function(xml){
-               
+               consle.log(xml);
            }
        },
        onError: { writtable: false, configurable: false, enumerable: false,
