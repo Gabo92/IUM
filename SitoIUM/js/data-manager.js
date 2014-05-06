61f24293189a31ac5693e1dca4698d29ddfe7b92
Groza Sergiu <serioja90@gmail.com>
2014-05-06 11:37:41 +0200

    Implementato il parser che genera una hash con all'interno tutti i dati.

--- SitoIUM/js/data-manager.js
+++ SitoIUM/js/data-manager.js
@@ -1,8 +1,9 @@
 (function(){
     var DataManager = function(options){
         var configs = options || {};
-        this.events = configs["enevts"] || {};
+        this.events = configs["events"] || {};
         this.responsesCount = 0;
+        this.data = {};
         this.init();
     };
     
@@ -13,43 +14,64 @@
     ];
     
     var DataManagerPrototype = Object.defineProperties({},{
-       init: { writtable: false, configurable: false, enumerable: false,
-           value: function(){
-               this.getAllData();
-           }
-       },
-       trigger: { writtable: false, configurable: false, enumerable: false,
-           value: function(name,data){
-               var handler = this.events[name + ''];
-               if(handler && typeof handler === "function" ){
-                   handler(data);
-               }
-           }
-       },
-       getAllData: { writtable: false, configurable: false, enumerable: false,
-           value: function(){
+        init: { writtable: false, configurable: false, enumerable: false,
+            value: function(){
+                this.getAllData();
+            }
+        },
+        trigger: { writtable: false, configurable: false, enumerable: false,
+            value: function(name,data){
+                var handler = this.events[name + ''];
+                if(handler && typeof handler === "function" ){
+                    handler(data);
+                }else{
+                    console.log("Handler not found");
+                }
+            }
+        },
+        getAllData: { writtable: false, configurable: false, enumerable: false,
+            value: function(){
                 var that = this;
                 $(DataManager.NAMES).each(function(){
-                    getRowsFromVeryBeginningToNow(this,"tpellegr","DESC",that.onDataRecieved,that.onError);
+                    getRowsFromVeryBeginningToNow(
+                        this,
+                        "tpellegr",
+                        "DESC",
+                        function(xml){ that.onDataRecieved(xml); },
+                        function(statusError){ that.onError(statusError);}
+                    );
                 });
-           }
-       },
-       onDataRecieved: { writtable: false, configurable: false, enumerable: false,
-           value: function(xml){
-                $(xml).find('Riga').each(function(){
-                                 var Nome = $(this).find('Nome').text();
-                                 var Valore = $(this).find('Valore').text();
-                                 var Data = $(this).find('Data').text();
-                                 var Unita = $(this).find('Unita').text();
-
-                console.log("Nome: "+ Nome + "\nValore: " + Valore + "\nUnita " + Unita + "\nData: "+ Data);})
-           }
-       },
-       onError: { writtable: false, configurable: false, enumerable: false,
-           value: function(statusError){
-               console.error(statusError);
-           }
-       }
+            }
+        },
+        onDataRecieved: { writtable: false, configurable: false, enumerable: false,
+            value: function(xml){
+                var that = this;
+                $(xml).find("Riga").each(function(){
+                    var year = $(this).find("Data").text().split("-")[0] + '';
+                    var type = $(this).find("Tipo").text().replace(/à/g,"a");
+                    var unity = $(this).find("Unita").text();
+                    var value = $(this).find("Valore").text();
+                    that.data[year] = that.data[year] || {};
+                    that.data[year][type] = that.data[year][type] || {}
+                    that.data[year][type].unity = unity;
+                    that.data[year][type].values = that.data[year][type].values || [];
+                    that.data[year][type].values.push(value);
+                });
+                this.responsesCount++;
+                if(this.responsesCount === DataManager.NAMES.length){
+                    this.trigger("onReady",this.data);
+                }
+            }
+        },
+        onError: { writtable: false, configurable: false, enumerable: false,
+            value: function(statusError){
+                console.error(statusError);
+                this.responsesCount++;
+                if(this.responsesCount === DataManager.NAMES.length){
+                    this.trigger("onReady",this.data);
+                }
+            }
+        }
     });
     
     DataManager.prototype = DataManagerPrototype;
