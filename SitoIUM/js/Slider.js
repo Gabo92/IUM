2d40852e961aa7fd731b330f11228857264c0d88
gabriele <gabriele@157.138.190.142>
2014-05-08 12:47:49 +0200

    Merge origin/master
    Conflicts:
    	SitoIUM/js/x3dresources.js

--- SitoIUM/js/Slider.js
+++ SitoIUM/js/Slider.js
@@ -7,6 +7,51 @@ $(function() {
       value: select[ 0 ].selectedIndex + 1,
       slide: function( event, ui ) {
         select[ 0 ].selectedIndex = ui.value - 1;
+      
+        var defaultFocus = document.getElementById("defaultCamera").getAttribute("set_bind");
+        var innovazioneFocus = document.getElementById("innovazioneCamera").getAttribute("set_bind");
+        var governanceFocus = document.getElementById("governanceCamera").getAttribute("set_bind");
+        //var politichepersonaleFocus = document.getElementById("politichepersonaleCamera").getAttribute("set_bind");
+        //var politicheStudentifocus = document.getElementById("politichestudentiCamera").getAttribute("set_bind");
+        var materialiFocus = document.getElementById("materialiCamera").getAttribute("set_bind");
+        //var energiaFocus = document.getElementById("energiaCamera").getAttribute("set_bind");
+        var acquaFocus = document.getElementById("acquaCamera").getAttribute("set_bind");
+        //var rifiutiFocus = document.getElementById("rifiutiCamera").getAttribute("set_bind");
+        //var supplychainFocus = document.getElementById("supplychainCamera").getAttribute("set_bind");
+        //var mobilitaFocus = document.getElementById("mobilitaCamera").getAttribute("set_bind");
+
+        if(defaultFocus == "false"){
+            if(innovazioneFocus == "true"){
+                cameraInnovazione();
+            }
+            else if(governanceFocus == "true"){
+                cameraGovernance();
+            }
+            /*else if(politichepersonaleFocus == "true"){
+                cameraPolitichePersonale();
+            }
+            else if(politicheStudentifocus == "true"){
+                cameraPoliticheStudenti();
+            }*/
+            else if(materialiFocus == "true"){
+                cameraMateriali();
+            }
+            /*else if(energiaFocus == "true"){
+                cameraEnergia();
+            }*/
+            else if(acquaFocus == "true"){
+                cameraAcqua();
+            }
+            /*else if(rifiutiFocus == "true"){
+                cameraRifiuti();
+            }
+            else if(supplychainFocus == "true"){
+                cameraSupplyChain();
+            }
+            else if(mobilitaFocus == "true"){
+                cameraMobilita();
+            }*/
+        }
       }
     });
     $( "#anni" ).change(function() {
